1ca52faaaee2623815d1b3a7006a9c48675e14fb
Maks <116167012+Str0ng3r@users.noreply.github.com>
2023-10-01 12:59:03 +0200

    API RESOURCE
    API ACCEPTED

--- src/components/works/works.jsx
+++ src/components/works/works.jsx
@@ -1,16 +1,34 @@
 import { useState } from "react";
 import { MySwiper } from "./swiper";
 import styles from "./work.module.css";
-import cross from '../../img/cross.png'
+import cross from "../../img/cross.png";
+import axios from "axios";
 export const Work = () => {
   const [modal, setModal] = useState(false);
+  const [name, setName] = useState(null);
+  const [emailuser, setEmailUser] = useState(null);
+  const [socialName, setSocialName] = useState(null);
+
   return (
     <section className={styles.sect}>
       <div className={styles.cont}>
         {modal && (
           <div className={styles.modal_window}>
-            <form action="" className={styles.form_modal}>
-                <button className={styles.cross} onClick={()=> {setModal(false)}}><img src={cross} alt="cross"  className={styles.img_cross}/></button>
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                setModal(false);
+              }}
+              className={styles.form_modal}
+            >
+              <button
+                className={styles.cross}
+                onClick={() => {
+                  setModal(false);
+                }}
+              >
+                <img src={cross} alt="cross" className={styles.img_cross} />
+              </button>
               <label htmlFor="name" className={styles.label_name}>
                 Ім'я
                 <input
@@ -19,6 +37,9 @@ export const Work = () => {
                   id="name"
                   placeholder="Ім'я"
                   className={styles.input_name}
+                  onChange={(e) => {
+                    setName(e.target.value);
+                  }}
                 />
               </label>
 
@@ -30,6 +51,9 @@ export const Work = () => {
                   id="email"
                   placeholder="namelastname@gmail.com"
                   className={styles.input_email}
+                  onChange={(e) => {
+                    setEmailUser(e.target.value);
+                  }}
                 />
               </label>
               <label htmlFor="social" className={styles.label_social}>
@@ -40,11 +64,27 @@ export const Work = () => {
                   id="social"
                   placeholder="@named"
                   className={styles.input_social}
+                  onChange={(e) => {
+                    setSocialName(e.target.value);
+                  }}
                 />
               </label>
-              <p className={styles.text_call}>Після відправки заявки,протягом двох днів з вами зв'яжуться для подальшої співпраці</p>
-              <button type="sumbit" className={styles.button_send}>Відправити</button>
-
+              <p className={styles.text_call}>
+                Після відправки заявки,протягом двох днів з вами зв'яжуться для
+                подальшої співпраці
+              </p>
+              <button
+                type="submit"
+                className={styles.button_send}
+                onClick={() => {
+                  axios.post(
+                    "https://6501abed736d26322f5c1aaa.mockapi.io/orders/application",
+                    { name: name, email: emailuser, social: socialName }
+                  );
+                }}
+              >
+                Відправити
+              </button>
             </form>
           </div>
         )}
