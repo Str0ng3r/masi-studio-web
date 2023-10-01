ab1533b1f7b4dbc44ba46574321ff6fe4528bdea
Maks <116167012+Str0ng3r@users.noreply.github.com>
2023-10-01 17:35:32 +0200

    HEADER UPDATED

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -4,6 +4,7 @@ export const Header = () => {
   return (
       <header className={styles.header}>
 <img src={logo} alt="logotip" className={styles.img_logo}/>
+<h2 className={styles.main_name}>asi</h2>
       </header>   
   );
 }
